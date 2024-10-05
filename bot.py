d89dd348e40b9c5982369f0dfdaf497e56a33f54
adymar73583418 <adymarr2005@gmail.com>
2024-10-05 19:42:04 +0600

    ewf

--- bot.py
+++ bot.py
@@ -56,7 +56,7 @@ async def send_messages():
             except Exception as e:
                 print(f"Не удалось отправить сообщение пользователю {name}: {e}")
             
-            time.sleep(random.randint(600-900))  # Задержка в 2 секунды между отправками
+            time.sleep(random.randint(600,900))  # Задержка в 2 секунды между отправками
 # Основной блок программы
 with client:
     client.loop.run_until_complete(send_messages())
